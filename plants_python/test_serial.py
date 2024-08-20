29a781f99b3e7bc70ebd3ad2aca60d4f79572f78
Zer Der <eder_zer@hotmail.com>
2024-08-19 23:24:38 -0600

    Adding project pictures

--- plants_python/test_serial.py
+++ plants_python/test_serial.py
@@ -4,38 +4,43 @@ import serial
 import json
 import numpy as np
 import cv2 as cv
+from datetime import datetime
 
 ser = serial.Serial('COM6', timeout=15)  # open serial port
 while True:
     try:
         msg = ser.readline().decode(encoding="utf-8", errors="ignore")
         msg = msg.replace("'", '"')
-        json.loads(msg)
+        msg = '{"date":' + f'"{datetime.now()}"' + ', "plants":' + msg + '}'
+        msg = json.loads(msg)
         print(msg)
-        cap = cv.VideoCapture(0)
-        if not cap.isOpened():
-            print("Cannot open camera")
-            exit()
-        while True:
-            # Capture frame-by-frame
-            ret, frame = cap.read()
+        data = []
+        with open('data_storage.json', 'a') as json_file:
+            json.dump(msg, json_file,
+                      indent=4,
+                      separators=(',', ': '))
+
+        count = 0
+        cap = cv.VideoCapture(0)  # video capture source camera (Here webcam of laptop)
+        ret, frame = cap.read()  # return a single frame in variable `frame`
 
             # if frame is read correctly ret is True
-            if not ret:
-                print("Can't receive frame (stream end?). Exiting ...")
-                break
-            # Our operations on the frame come here
-            gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-            color = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-            # Display the resulting frame
-            cv.imshow('frame', frame)
-            if cv.waitKey(1) == ord('q'):
-                break
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        date = datetime.now()
+        date = f"{date}".replace('.','-').replace(' ','').replace(':','-')
+        name = f"{date}.jpg"
+        print(name)
+        #cv.imwrite(name, frame)
+        count += 1
 
         # When everything done, release the capture
         cap.release()
-        cv.destroyAllWindows()
+
     except Exception as e:
         print(e)
+
 ser.close()
+
 
